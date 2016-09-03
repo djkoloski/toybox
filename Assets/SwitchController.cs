8a6cab62d6a30ab30c3c0d1b66fdeaadaa98e7e7
Ryan O'Malley <ryeomalley@gmail.com>
2016-09-03 00:12:48 -0400

    Added the basis for the actual toybox.

---
+++ Assets/SwitchController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchController : MonoBehaviour {
+
+	SpringJoint thisSpring;
+
+	Vector3 initialPosition;
+	public bool isOn = false;
+
+	// Use this for initialization
+	void Start () {
+		initialPosition = transform.position;
+		thisSpring = GetComponent<SpringJoint> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((initialPosition - transform.position).magnitude > 0.4)
+			isOn = true;
+		else
+			isOn = false;
+	}
+}
